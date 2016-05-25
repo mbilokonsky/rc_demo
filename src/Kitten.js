59c26f8cb5eae2c211fb9f5af82c81062ddcc9d2
myk bilokonsky <mbilokonsky@gmail.com>
2016-05-25 18:53:03 -0400

    First commit, week 1

---
+++ src/Kitten.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Kitten extends React.Component {
+  render() {
+    return <img src="http://placekitten.com/320/240" />
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import Kitten from './Kitten';
+
+console.log("Hello!");
+
+reactDOM.render(
+  <Kitten />,
+  document.getElementById('root')
+);
