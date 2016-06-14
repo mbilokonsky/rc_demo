4bf97450476c285239b2814bb40e159cbe75a372
myk bilokonsky <mbilokonsky@gmail.com>
2016-06-14 15:53:26 -0400

    brought over redux code from tutorial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import reactDOM from 'react-dom';
-import Kitten from './Kitten';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-console.log("Hello!");
+let store = createStore(todoApp)
 
-reactDOM.render(
-  <Kitten />,
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
